43088ae573e02c8cfd111c4daf3d691097d30baa
Alex <shaftoe@users.noreply.github.com>
2021-06-23 08:30:17 +0200

    fix: update Lambda name

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const version = require('child_process')
     .execSync('git rev-parse HEAD')
     .toString().trim()
 const vars = {
-    LAMBDA_NAME: "notifications-notificationslambdamailjetF3B685B6-1979LDYKMT4WK",
+    LAMBDA_NAME: "notifications-notificationslambdamailjetF3B685B6-uBqHEo0igou8",
     LAST_UPDATE: new Date().toISOString(),
-    REGION: "us-east-2",
+    REGION: "us-east-1",
     VERSION_COMMIT: `https://github.com/shaftoe/webmail/commit/${version}`,
     VERSION: version,
 }
